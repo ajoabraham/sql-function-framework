a8920a1184a0e6800a94cccdfe75eb3bf72389e8
Alexey Paramonov <a.paramonov@gmail.com>
2014-01-02 17:49:47 +0100

    validation + commentaries

--- src/main/java/frmw/model/Formula.java
+++ src/main/java/frmw/model/Formula.java
@@ -1,6 +1,7 @@
 package frmw.model;
 
 import frmw.dialect.Dialect;
+import frmw.model.exception.SQLFrameworkException;
 import frmw.model.position.PositionHolder;
 import frmw.parser.Parsing;
 import org.codehaus.jparsec.error.ParserException;
@@ -12,20 +13,36 @@ public class Formula {
 
 	private final FormulaElement root;
 
-	public Formula(String formula, Parsing parsing) {
+	/**
+	 * @throws SQLFrameworkException on any error during parsing
+	 */
+	public Formula(String formula, Parsing parser) throws SQLFrameworkException {
 		PositionHolder.INST.currentFormula(this);
 
 		try {
-			this.root = parsing.parse(formula);
+			this.root = parser.parse(formula);
 		} catch (ParserException e) {
 			throw e;
 //			throw new ParsingException();
 		}
 	}
 
-	public String sql(Dialect dialect) {
+	/**
+	 * @return sql to appropriate database dialect
+	 * @throws SQLFrameworkException on any error
+	 */
+	public String sql(Dialect dialect) throws SQLFrameworkException {
 		StringBuilder sb = new StringBuilder();
 		root.sql(dialect, sb);
 		return sb.toString();
+	}
+
+	/**
+	 * Validates formula for errors.
+	 *
+	 * @throws SQLFrameworkException if validation was failed
+	 */
+	public void validate(Dialect dialect) throws SQLFrameworkException {
+		sql(dialect);
 	}
 }
